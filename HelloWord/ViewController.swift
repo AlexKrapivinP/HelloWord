87b72e9ea7c6b43b5e8ca87d9eb3e0b94ebf9e53
Alex <krapivin.aleksej@bk.ru>
2024-05-14 20:39:04 +0300

    Setup UI

--- HelloWord/ViewController.swift
+++ HelloWord/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var showGreetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showGreetingButtonDitTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+        
+    }
+    
 }
 
